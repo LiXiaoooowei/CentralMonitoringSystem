3e6cde388467796303b70f1485e1d7a60088abab
LiXiaoooowei <lixiaowei0535@gmail.com>
2017-07-05 17:08:21 +0800

    add firebase config to env production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDBSr1JH-MLoinouS1iKnT-_MMC0g3RKYM',
+    authDomain: 'centralmonitoringsystem-1a619.firebaseapp.com',
+    databaseURL: 'https://centralmonitoringsystem-1a619.firebaseio.com',
+    projectId: 'centralmonitoringsystem-1a619',
+    storageBucket: 'centralmonitoringsystem-1a619.appspot.com',
+    messagingSenderId: '955806803705'
+  }
 };
