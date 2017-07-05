e3a14fad603bb385f90823baf66572e4b2d53c47
LiXiaoooowei <lixiaowei0535@gmail.com>
2017-07-05 17:01:05 +0800

    link trade records with firebase

--- src/app/CreateTradeRecord/create-trade-record.component.ts
+++ src/app/CreateTradeRecord/create-trade-record.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-popup',
@@ -11,8 +14,11 @@ export class CreateTradeRecordComponent {
   @Input() tradeTime: String;
   @Input() tradeSymbol: String;
   @Input() tradeAmt: String;
+
   public visible = false;
   public visibleAnimate = false;
+
+  constructor(public afAuth: AngularFireAuth) {}
 
   public show(): void {
     this.visible = true;
@@ -26,6 +32,12 @@ export class CreateTradeRecordComponent {
 
   public add(): void {
     console.log(this.tradeTime + ' ' + this.tradeSymbol + ' ' + this.tradeAmt);
+    const userRef = firebase.database().ref().child('traders').child(this.afAuth.auth.currentUser.uid);
+    userRef.child('records').push({
+      'amount': this.tradeAmt,
+      'symbol': this.tradeSymbol,
+      'time': this.tradeTime
+    });
     this.hide();
   }
 
--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 import {Router} from '@angular/router';
 
 import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
